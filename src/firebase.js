7591b262d7cddfd157d0f0125d87944c110dee05
KSI Olajide Olatunji <manan.vinod.shah@gmail.com>
2020-11-06 13:16:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDADlzYSp3yJfh53YnJDAlq3gQfc28Iw7M",
-  authDomain: "tik-tok-clone-e1828.firebaseapp.com",
-  databaseURL: "https://tik-tok-clone-e1828.firebaseio.com",
-  projectId: "tik-tok-clone-e1828",
-  storageBucket: "tik-tok-clone-e1828.appspot.com",
-  messagingSenderId: "1044878935298",
-  appId: "1:1044878935298:web:3aa06e369e3f34aa17ee1b",
-  measurementId: "G-9PYF6E3L0W"
+  //paste your own firebase config file
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
